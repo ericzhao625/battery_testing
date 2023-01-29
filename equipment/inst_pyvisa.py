a507d5eb26b2e5e47c2944e66b0a9449451b05f9
Eric Zhao <eric.zhao@uwaterloo.ca>
2023-01-29 18:09:51 -0500

    Updated base PyVisa instrument class to save the IDN values to specific variables. Also updated to support PyVisa instruments that don't support *IDN? commands.

--- equipment/inst_pyvisa.py
+++ equipment/inst_pyvisa.py
@@ -13,11 +13,20 @@ class PyVisaInstrument:
 
     Attributes:
         inst: PyVISA resource instance.
-        idn: Identifying details of the instrument.
+        manufacturer: Manufacturer name of instrument.
+        model_number: Model number of instrument.
+        serial_number: Serial number of instrument.
+        firmware_level: Firmware level of instrument.
     """
     def __init__(self, visa_name: str) -> None:
         rm = pyvisa.ResourceManager()
         self.inst = rm.open_resource(visa_name)
-        self.idn = self.inst.query('*IDN?').split(', ')
-        self.model_number = self.idn[1]
-        print(f"Connected to {self.idn[0]} {self.idn[1]}.")
+        try:
+            idn = self.inst.query('*IDN?').split(', ')
+            self.manufacturer = idn[0]
+            self.model_number = idn[1]
+            self.serial_number = idn[2]
+            self.firmware_level = idn[3]
+            print(f"Connected to {idn[0]} {idn[1]}.")
+        except pyvisa.errors.VisaIOError:
+            print(f"Connected to {visa_name}.")
