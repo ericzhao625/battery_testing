bc22c5f0f18d19cd0a12a30187a544e7547d3338
Eric Zhao <eric.zhao@uwaterloo.ca>
2023-01-30 20:53:03 -0500

    Removed firmware and software version (didn't seem necessary).

--- equipment/inst_pyvisa.py
+++ equipment/inst_pyvisa.py
@@ -15,8 +15,6 @@ class PyVisaInstrument:
         inst: PyVISA resource instance.
         manufacturer: Manufacturer name of instrument.
         model_number: Model number of instrument.
-        serial_number: Serial number of instrument.
-        firmware_level: Firmware level of instrument.
     """
     def __init__(self, visa_name: str) -> None:
         rm = pyvisa.ResourceManager()
@@ -25,8 +23,6 @@ class PyVisaInstrument:
             idn = self.inst.query('*IDN?').split(', ')
             self.manufacturer = idn[0]
             self.model_number = idn[1]
-            self.serial_number = idn[2]
-            self.firmware_level = idn[3]
             print(f"Connected to {idn[0]} {idn[1]}.")
         except pyvisa.errors.VisaIOError:
             print(f"Connected to {visa_name}.")
